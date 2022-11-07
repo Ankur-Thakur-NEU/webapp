f249e38fb994bab7445fba1ffd95465ebeb5a46d
Ankur-Thakur-NEU <thakur.ank@northeastern.edu>
2022-11-07 00:37:25 -0500

    Logged changes

--- WebApp.CloudApi/Controllers/AccountController.cs
+++ WebApp.CloudApi/Controllers/AccountController.cs
@@ -45,6 +45,8 @@ public class AccountController : ControllerBase
         {
             try
             {
+                string connectionString = $"Host={GlobalData.Application.Where(s => s.Key == "Database").FirstOrDefault().Value};Database={GlobalData.Application.Where(s => s.Key == "DatabaseName").FirstOrDefault().Value};Port={GlobalData.Application.Where(s => s.Key == "DatabasePort").FirstOrDefault().Value};Username={GlobalData.Application.Where(s => s.Key == "MasterUsername").FirstOrDefault().Value};Password={GlobalData.Application.Where(s => s.Key == "MasterPassword").FirstOrDefault().Value};";
+                _logger.LogInformation(connectionString);
                 var existingAccount = _db.GetAccount(account.Email);
                 if (existingAccount.Email == account.Email)
                 {
